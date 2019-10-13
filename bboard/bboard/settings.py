8999baaaed2d3000856df869e8097ec704663e5b
Antony343 <38049931+Antony343@users.noreply.github.com>
2019-10-14 00:21:20 +0200

    Update settings.py

--- bboard/bboard/settings.py
+++ bboard/bboard/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = '/static/'
 # SMTP settings
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'knijniymir531@gmail.com'
+EMAIL_HOST_USER = '****@gmail.com'
 EMAIL_USE_TLS = True
 # Grant access to your account for unreliable apps firstly in gmail account settings
-EMAIL_HOST_PASSWORD = 'knyaztrue749'
+EMAIL_HOST_PASSWORD = '****'
